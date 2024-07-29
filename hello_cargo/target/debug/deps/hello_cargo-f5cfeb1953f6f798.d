302bab396d81727b03cd2e877055c23b50b5586c
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-07-29 15:52:28 +0200

    Cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-f5cfeb1953f6f798.d
@@ -0,0 +1,5 @@
+/Users/kellysmulian/GitHub/learn-rust/hello_cargo/target/debug/deps/hello_cargo-f5cfeb1953f6f798: src/main.rs
+
+/Users/kellysmulian/GitHub/learn-rust/hello_cargo/target/debug/deps/hello_cargo-f5cfeb1953f6f798.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/kellysmulian/GitHub/learn-rust/hello_cargo/target/debug/hello_cargo: /Users/kellysmulian/GitHub/learn-rust/hello_cargo/src/main.rs
