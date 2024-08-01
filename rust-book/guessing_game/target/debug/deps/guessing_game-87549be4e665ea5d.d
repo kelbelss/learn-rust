4c691e50b4a2fd4f84768c78ce5f44deedeb59ee
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-01 15:40:54 +0200

    cargo

---
+++ rust-book/guessing_game/target/debug/deps/guessing_game-87549be4e665ea5d.d
@@ -0,0 +1,5 @@
+/Users/kellysmulian/GitHub/learn-rust/rust-book/guessing_game/target/debug/deps/libguessing_game-87549be4e665ea5d.rmeta: src/main.rs
+
+/Users/kellysmulian/GitHub/learn-rust/rust-book/guessing_game/target/debug/deps/guessing_game-87549be4e665ea5d.d: src/main.rs
+
+src/main.rs:
---
+++ rust-book/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/kellysmulian/GitHub/learn-rust/rust-book/guessing_game/target/debug/guessing_game: /Users/kellysmulian/GitHub/learn-rust/rust-book/guessing_game/src/main.rs
