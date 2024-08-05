c5bbec97f4dfd53234335c2ea7fcd17a3a295670
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-05 13:09:01 +0200

    Variables - let, mut, const, shadowing

---
+++ rust-book/variables/target/debug/deps/variables-59d052b6613a41cb.d
@@ -0,0 +1,5 @@
+/Users/kellysmulian/GitHub/learn-rust/rust-book/variables/target/debug/deps/variables-59d052b6613a41cb: src/main.rs
+
+/Users/kellysmulian/GitHub/learn-rust/rust-book/variables/target/debug/deps/variables-59d052b6613a41cb.d: src/main.rs
+
+src/main.rs:
---
+++ rust-book/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/kellysmulian/GitHub/learn-rust/rust-book/variables/target/debug/variables: /Users/kellysmulian/GitHub/learn-rust/rust-book/variables/src/main.rs
