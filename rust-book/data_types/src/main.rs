1153e6e630ed75f3ae354e3e90f7ecef5068482c
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-05 15:58:34 +0200

    Data Types

---
+++ rust-book/data_types/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    // Scalar and Compound
+
+    // Scalar
+    // - Integer Types
+    // - Floating-Point Types
+    // - Numeric Operations
+    // - Booleans Types
+    // - Character Types
+
+    // Compound
+    // - The Tuple Type
+    // - The Array Type
+}
