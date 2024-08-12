5ca62ace2cf7a724130c0214f8d3966247e73503
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-12 13:47:34 +0200

    Rustlings 03_if Solutions

---
+++ rustlings/03_if/if3.rs
@@ -0,0 +1,55 @@
+fn animal_habitat(animal: &str) -> &str {
+    // TODO: Fix the compiler error in the statement below.
+    let identifier = if animal == "crab" {
+        "Beach"
+    } else if animal == "gopher" {
+        "Burrow"
+    } else if animal == "snake" {
+        "Desert"
+    } else {
+        "Unknown"
+    };
+
+    // Don't change the expression below!
+    if identifier == "Beach" {
+        "Beach"
+    } else if identifier == "Burrow" {
+        "Burrow"
+    } else if identifier == "Desert" {
+        "Desert"
+    } else {
+        "Unknown"
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+    let test = animal_habitat("crab");
+    println!("{test}");
+}
+
+// Don't change the tests!
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gopher_lives_in_burrow() {
+        assert_eq!(animal_habitat("gopher"), "Burrow")
+    }
+
+    #[test]
+    fn snake_lives_in_desert() {
+        assert_eq!(animal_habitat("snake"), "Desert")
+    }
+
+    #[test]
+    fn crab_lives_on_beach() {
+        assert_eq!(animal_habitat("crab"), "Beach")
+    }
+
+    #[test]
+    fn unknown_animal() {
+        assert_eq!(animal_habitat("dinosaur"), "Unknown")
+    }
+}
