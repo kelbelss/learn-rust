50876c1a81c4fe4bc3c73b2e80f1deb19cd8f52e
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-26 14:05:15 +0200

    add memoisation and the time complexity

--- rust-book/common_programming_concepts/fibonacci/src/main.rs
+++ rust-book/common_programming_concepts/fibonacci/src/main.rs
@@ -1,31 +1,31 @@
+use std::collections::HashMap;
 use std::time::Instant;
 
 fn main() {
     println!("Fibonacci with time measurements");
 
-    // The recursive method's time will increase exponentially = O(2^n) time complexity.
     // The non-recursive method's time will increase roughly linearly with n = O(n) time complexity.
+    // The recursive method's time will increase exponentially = O(2^n) time complexity.
+    // The memoisation method with resursion will reduce the time complexity of the recursive function from O(2^n) to O(n).
 
     for i in 0..=50 {
-        let start_recursive = Instant::now();
-        let recursive_results = recursive_fibonacci(i);
-        let duration_recursive = start_recursive.elapsed();
-
         let start_non_recursive = Instant::now();
         let non_recursive_results = fib_non_recursive(i);
         let duration_non_recursive = start_non_recursive.elapsed();
 
-        println!(
-            "n = {}: Recursive = {} (in {:?}), Non-recursive = {} (in {:?})",
-            i, recursive_results, duration_recursive, non_recursive_results, duration_non_recursive
-        );
-    }
-}
+        let start_recursive = Instant::now();
+        let recursive_results = recursive_fibonacci(i);
+        let duration_recursive = start_recursive.elapsed();
 
-fn recursive_fibonacci(n: u32) -> u128 {
-    match n {
-        0 | 1 => 1,
-        _ => recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2),
+        let mut memo = HashMap::new(); // Initialise a memoisation table
+        let start_memoisation = Instant::now();
+        let memoisation_results = memoisation_recursive_fib(i, &mut memo);
+        let duration_memoisation = start_memoisation.elapsed();
+
+        println!(
+            "n = {}: Non-recursive = {} (in {:?}), Recursive = {} (in {:?}), Memoisation = {} (in {:?})",
+            i, non_recursive_results, duration_non_recursive, recursive_results, duration_recursive, memoisation_results, duration_memoisation
+        );
     }
 }
 
@@ -44,4 +44,25 @@ fn fib_non_recursive(n: u32) -> u128 {
     }
 
     b
+}
+
+fn recursive_fibonacci(n: u32) -> u128 {
+    match n {
+        0 | 1 => 1,
+        _ => recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2),
+    }
+}
+
+fn memoisation_recursive_fib(n: u32, memo: &mut HashMap<u32, u128>) -> u128 {
+    if let Some(&result) = memo.get(&n) {
+        return result;
+    }
+
+    let result = match n {
+        0 | 1 => 1,
+        _ => memoisation_recursive_fib(n - 1, memo) + memoisation_recursive_fib(n - 2, memo),
+    };
+
+    memo.insert(n, result);
+    result
 }
